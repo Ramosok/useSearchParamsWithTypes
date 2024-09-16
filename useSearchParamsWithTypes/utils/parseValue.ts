24e2fe067bdb9f27fba6ecd2e87f28d1a705838f
Vitaliy <79109670+Ramosok@users.noreply.github.com>
2024-09-16 10:17:53 +0300

    Add files via upload

--- useSearchParamsWithTypes/utils/parseValue.ts
+++ useSearchParamsWithTypes/utils/parseValue.ts
@@ -1,8 +1,8 @@
 import { ParseValueReturn } from "../types";
 
 export const parseValue = (value: string): ParseValueReturn => {
-    if (value === 'true') return true;
-    if (value === 'false') return false;
-    const num = Number(value);
-    return isNaN(num) ? value : num;
-}
+  if (value === "true") return true;
+  if (value === "false") return false;
+  const num = Number(value);
+  return isNaN(num) ? value : num;
+};
